2f18497996d3a7c4366fd8397a3fb18046c502bd
IsaacHuo <2210286979@qq.com>
2025-11-12 20:11:06 +0800

    Fix rollup dependency issue in Cloudflare build

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Cloudflare Pages build script
 
+# Remove node_modules and package-lock.json to fix rollup optional dependency issue
+rm -rf node_modules package-lock.json
+
 # Install dependencies
-npm install --legacy-peer-deps
+npm install
 
 # Build the project
 npm run build
